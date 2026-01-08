70c620bd7cb83acef79faf4dcf5abf16dad5c7e6
win401 <winnerv401@gmail.com>
2026-01-09 03:58:01 +0900

    레이아웃 코드 작성

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "SUNGWOO | Web Publisher",
+  description: "Advanced Web Publisher portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
